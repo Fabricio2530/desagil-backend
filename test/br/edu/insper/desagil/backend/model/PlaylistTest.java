4f34040c72b62bbceb42323baf35f12d6dd8ccca
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2021-06-14 16:39:45 -0300

    PlaylistTest passando

--- test/br/edu/insper/desagil/backend/model/PlaylistTest.java
+++ test/br/edu/insper/desagil/backend/model/PlaylistTest.java
@@ -2,6 +2,11 @@ package br.edu.insper.desagil.backend.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,21 +19,58 @@ class PlaylistTest {
 
 	@Test
 	void testRoundDownToZero() {
-		assertEquals(true, false);
+		Artist loona = new Artist("LOONA");
+		Track butterfly = new Track(loona, "Butterfly", 255);
+		
+		Playlist maisOuvidas = new Playlist(0);
+		maisOuvidas.addTrack(butterfly);
+		maisOuvidas.putRating("Joao", 2);
+		maisOuvidas.putRating("Jonas", 1);
+		maisOuvidas.putRating("Ana", 3);
+		maisOuvidas.putRating("Eduardo", 3);
+		assertEquals(2.0, maisOuvidas.averageRatings(), DELTA);
 	}
 
 	@Test
 	void testRoundUpToHalf() {
-		assertEquals(true, false);
+		Artist loona = new Artist("LOONA");
+		Track butterfly = new Track(loona, "Butterfly", 255);
+		
+		Playlist maisOuvidas = new Playlist(0);
+		maisOuvidas.addTrack(butterfly);
+		maisOuvidas.putRating("Joao", 2);
+		maisOuvidas.putRating("Jonas", 1);
+		maisOuvidas.putRating("Eduardo", 1);
+		
+		assertEquals(1.5, maisOuvidas.averageRatings(), DELTA);
 	}
 
 	@Test
 	void testRoundDownToHalf() {
-		assertEquals(true, false);
+		Artist loona = new Artist("LOONA");
+		Track butterfly = new Track(loona, "Butterfly", 255);
+		
+		Playlist maisOuvidas = new Playlist(0);
+		maisOuvidas.addTrack(butterfly);
+		maisOuvidas.putRating("Joao", 2);
+		maisOuvidas.putRating("Jonas", 2);
+		maisOuvidas.putRating("Eduardo", 1);
+		
+		assertEquals(1.5, maisOuvidas.averageRatings(), DELTA);
 	}
 
 	@Test
 	void testRoundUpToOne() {
-		assertEquals(true, false);
+		Artist loona = new Artist("LOONA");
+		Track butterfly = new Track(loona, "Butterfly", 255);
+		
+		Playlist maisOuvidas = new Playlist(0);
+		maisOuvidas.addTrack(butterfly);
+		maisOuvidas.putRating("Joao", 2);
+		maisOuvidas.putRating("Jonas", 1);
+		maisOuvidas.putRating("Eduardo", 1);
+		maisOuvidas.putRating("Fabricio", 3);
+		
+		assertEquals(2.0, maisOuvidas.averageRatings(), DELTA);
 	}
 }
